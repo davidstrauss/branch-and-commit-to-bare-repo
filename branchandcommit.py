73c7c9b87f3513253687204d4f915bf2f9a9336c
David Strauss <david@davidstrauss.net>
2015-05-15 14:50:36 -0700

    Abstract commit creation.

--- branchandcommit.py
+++ branchandcommit.py
@@ -1,6 +1,16 @@
 import tempfile
 import pygit2
-import time
+
+def add_commit(repo, ref, msg, author, path=None, content=None, parent=None):
+    tb = repo.TreeBuilder()
+    if path is not None:
+        blob = repo.create_blob(file_content)
+        tb.insert(file_path, blob, pygit2.GIT_FILEMODE_BLOB)
+    parents = []
+    if parent is not None:
+        parents = [parent]
+    precommit = tb.write()
+    commit = repo.create_commit(ref, author, author, msg, precommit, parents)
 
 # Set incoming parameters. These would be in the web request.
 
@@ -12,38 +22,26 @@ file_path = 'hello.txt'
 file_content = 'Hello, world!'
 commit_message = 'Adding hello.txt.'
 
-# Preliminary setup for proof-of-concept purposes
+# Create an author.
+
+author = pygit2.Signature(committer_name, committer_email)
+
+# Preliminary setup for proof-of-concept purposes. Creates a master
+# branch with a single, empty commit.
 
 tmpdir = tempfile.TemporaryDirectory(suffix='.git', prefix='branchandcommit-')
 print('Creating bare repository: {}'.format(tmpdir.name))
 repo = pygit2.init_repository(tmpdir.name, True)
+add_commit(repo, 'refs/heads/master', 'Initial commit.', author)
 
 # Create the branch (if non-existent).
 
 print('Creating branch: {}'.format(branch_name))
 
-# Create a git blob with the file content.
+# Commit the change.
 
-print('Creating blob with content: {}'.format(file_content))
-
-blob = repo.create_blob(file_content)
-
-# Create the tree with the file.
-
-print('Creating tree for file: {}'.format(file_path))
-
-tb = repo.TreeBuilder()
-tb.insert(file_path, blob, pygit2.GIT_FILEMODE_BLOB)
-
-# Creating author.
-
-author = pygit2.Signature(committer_name, committer_email, int(time.time()))
-
-# Commit the file to the branch.
-
-print('Creating commit: {}'.format(commit_message))
-
-precommit = tb.write()
-commit = repo.create_commit('refs/heads/master', author, author, commit_message, precommit, [])
+#parent = repo.head.target
+#parent = None
+add_commit(repo, 'refs/heads/master', commit_message, author, file_path, file_content, repo.head.target)
 
 input("Press Enter to clean up the temporary git repository and exit...")
